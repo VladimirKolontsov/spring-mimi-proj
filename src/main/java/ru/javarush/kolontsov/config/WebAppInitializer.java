209152bbb7878a65d35ca39924ef33cc85a4b2c8
Vladimir Kolontsov <kolontsovvova@mail.ru>
2023-04-16 14:32:03 +0700

    16.04 spring-mini-project

---
+++ src/main/java/ru/javarush/kolontsov/config/WebAppInitializer.java
@@ -0,0 +1,18 @@
+package ru.javarush.kolontsov.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/ru/javarush/kolontsov/domain/Status.java
@@ -0,0 +1,7 @@
+package ru.javarush.kolontsov.domain;
+
+public enum Status {
+    IN_PROGRESS,
+    DONE,
+    PAUSED
+}
